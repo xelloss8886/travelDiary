997bdd45151b84c39a4c27f583b4786e3c0ad4c2
xelloss8886 <xelloss8886@gmail.com>
2018-05-29 23:24:36 +0900

    Added file.

--- core/src/main/java/com/travel/diary/service/DiaryService.java
+++ core/src/main/java/com/travel/diary/service/DiaryService.java
@@ -2,7 +2,7 @@ package com.travel.diary.service;
 
 import com.travel.diary.entity.DiaryEntity;
 
-public interface DiaryService {
+public interface DiaryService extends GeneralService<DiaryEntity> {
 
 	public void add(DiaryEntity diary);
 }
---
+++ core/src/main/java/com/travel/diary/service/GeneralService.java
@@ -0,0 +1,5 @@
+package com.travel.diary.service;
+
+public interface GeneralService<T> {
+	public void add(T entity);
+}
