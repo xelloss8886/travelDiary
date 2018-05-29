a2a69922d5ccec306a22deb32230e0d083ad49f6
xelloss8886 <xelloss8886@gmail.com>
2018-05-29 23:13:42 +0900

    Added response body.

--- core/src/main/java/com/travel/diary/controller/HomeController.java
+++ core/src/main/java/com/travel/diary/controller/HomeController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.travel.diary.entity.DiaryEntity;
 import com.travel.diary.service.DiaryService;
 
@@ -14,12 +16,15 @@ public class HomeController {
 	@Autowired
 	private DiaryService diaryService;
 
+	private ObjectMapper mapper = new ObjectMapper();
+
 	@GetMapping("/")
-	public String home(Model model) {
+	@ResponseBody
+	public String home(Model model) throws Exception {
 		DiaryEntity entity = new DiaryEntity();
 		entity.setDiaryName("test123");
 		entity.setDiaryId("test1111");
 		diaryService.add(entity);
-		return "";
+		return mapper.writeValueAsString(entity);
 	}
 }
