f0468eefca979068523ea2c0c5da7fbff49c86c1
xelloss8886 <xelloss8886@gmail.com>
2018-07-01 13:00:15 +0900

    test

--- core/src/main/java/com/travel/diary/TravelApplication.java
+++ core/src/main/java/com/travel/diary/TravelApplication.java
@@ -2,8 +2,10 @@ package com.travel.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = "com.travel.diary")
+@EntityScan(basePackages = "com.travel.diary.entity")
 public class TravelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TravelApplication.class);
